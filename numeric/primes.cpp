419720dcd2d337f011fc88d219c99253fea4ca0b
Mr. A.N <69844241+mr-nazari@users.noreply.github.com>
2023-05-17 22:37:08 +0330

    Update primes.cpp

--- numeric/primes.cpp
+++ numeric/primes.cpp
@@ -21,3 +21,21 @@ void sieve(int n) {
         }
     }
 }
+
+bool is_prime(int n) {
+   if (n <= 1) {
+      return false;
+   }
+   if (n <= 3) {
+      return true;
+   }
+   if (n % 2 == 0 || n % 3 == 0) {
+      return false;
+   }
+   for (int d = 5; d * d <= n; d += 6) {
+      if (n % d == 0 || n % (d + 2) == 0) {
+         return false;
+      }
+   }
+   return true;
+}
