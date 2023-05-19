a552bb4004619a0b796d1a732ab2c6108e6577fb
Mr. A.N <69844241+mr-nazari@users.noreply.github.com>
2023-05-19 12:14:13 +0330

    Update extgcd.cpp: add diophantine function

--- numeric/extgcd.cpp
+++ numeric/extgcd.cpp
@@ -10,3 +10,24 @@ T extgcd(T a, T b, T& x, T& y) {
    x -= p * y;
    return g;
 }
+
+template <typename T>
+bool diophantine(T a, T b, T c, T& x, T& y, T& g) {
+   g = extgcd(abs(a), abs(b), x, y);
+   if (g == 0) {
+      // NOTE: Now x = 0 and y = 1.
+      return c == 0;
+   }
+   if (c % g != 0) {
+      return false;
+   }
+   x *= c / g;
+   y *= c / g;
+   if (a < 0) {
+      x *= -1;
+   }
+   if (b < 0) {
+      y *= -1;
+   }
+   return true;
+}
