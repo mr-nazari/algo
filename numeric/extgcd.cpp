82085b1b4223dcaa3d4ac13607207ccc69ba6dd6
Mr. A.N <69844241+mr-nazari@users.noreply.github.com>
2023-06-06 13:59:05 +0330

    Update extgcd.cpp: 2 space

--- numeric/extgcd.cpp
+++ numeric/extgcd.cpp
@@ -1,33 +1,33 @@
 template <typename T>
 T extgcd(T a, T b, T& x, T& y) {
-   if (a == 0) {
-      x = 0;
-      y = 1;
-      return b;
-   }
-   T p = b / a;
-   T g = extgcd(b - p * a, a, y, x);
-   x -= p * y;
-   return g;
+  if (a == 0) {
+    x = 0;
+    y = 1;
+    return b;
+  }
+  T p = b / a;
+  T g = extgcd(b - p * a, a, y, x);
+  x -= p * y;
+  return g;
 }
 
 template <typename T>
 bool diophantine(T a, T b, T c, T& x, T& y, T& g) {
-   g = extgcd(abs(a), abs(b), x, y);
-   if (g == 0) {
-      // NOTE: Now x = 0 and y = 1.
-      return c == 0;
-   }
-   if (c % g != 0) {
-      return false;
-   }
-   x *= c / g;
-   y *= c / g;
-   if (a < 0) {
-      x *= -1;
-   }
-   if (b < 0) {
-      y *= -1;
-   }
-   return true;
+  g = extgcd(abs(a), abs(b), x, y);
+  if (g == 0) {
+    // NOTE: Now x = 0 and y = 1.
+    return c == 0;
+  }
+  if (c % g != 0) {
+    return false;
+  }
+  x *= c / g;
+  y *= c / g;
+  if (a < 0) {
+    x *= -1;
+  }
+  if (b < 0) {
+    y *= -1;
+  }
+  return true;
 }
