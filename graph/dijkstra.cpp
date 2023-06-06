af3aabf2e2882cc5e82db0318ca35d591455f67f
Mr. A.N <69844241+mr-nazari@users.noreply.github.com>
2023-06-06 13:54:51 +0330

    Update dijkstra.cpp: 2 space

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -1,43 +1,53 @@
-template <typename C, bool directed = false>
-struct Dijkstra {
-   vector<vector<IncidentEdge<C>>> adj;
-   vector<C> dist;
+template <typename C, bool D = false>
+struct dijkstra {
+  template <typename T>
+  struct incident_edge {
+    int to;
+    T weight;
 
-   dijkstra(int n) : adj(n) {}
+    bool operator<(const incident_edge& other) const {
+      return pair(weight, to) < pair(other.weight, other.to);
+    }
+  }
 
-   C operator[](int node) {
-      return dist[node];
-   }
+  vector<vector<incident_edge<C>>> adj;
+  vector<C> dist;
 
-   void addEdge(int from, int to, C weight) {
-      adj[from].push_back({to, weight});
-      if (!directed) {
-         adj[to].push_back({from, weight});
+  dijkstra(int n) : adj(n) {}
+
+  C operator[](int node) {
+    return dist[node];
+  }
+
+  void add_edge(int from, int to, C weight) {
+    adj[from].push_back({to, weight});
+    if (!D) {
+      adj[to].push_back({from, weight});
+    }
+  }
+
+  void generate(vector<int> src) {
+    dist.assign((int) adj.size(), numeric_limits<C>::max());
+    using T = pair<C, int>;
+    priority_queue<T, vector<T>, greater<T>> pq;
+    auto ad = [&](int to, C cost) {
+      if (cost >= dist[to]) {
+        return;
       }
-   }
-
-   void generate(vector<int>& src) {
-      dist.assign((int) adj.size(), numeric_limits<C>::max());
-      using T = pair<C, int>;
-      priority_queue<T, vector<T>, greater<T>> pq;
-      auto ad = [&](int to, C cost) {
-         if (cost >= dist[to]) {
-            return;
-         }
-         pq.push({dist[to] = cost, to});
-      };
-      for (auto& s : src) {
-         ad(s, 0);
+      pq.push({dist[to] = cost, to});
+    };
+    for (auto && s : src) {
+      ad(s, 0);
+    }
+    while (!pq.empty()) {
+      auto [cd, node] = pq.top();
+      pq.pop();
+      if (cd > dist[node]) {
+        continue;
       }
-      while (!pq.empty()) {
-         auto [cd, node] = pq.top();
-         pq.pop();
-         if (cd > dist[node]) {
-            continue;
-         }
-         for (auto& [neighbor, weight] : adj[node]) {
-            ad(neighbor, cd + weight);
-         }
+      for (auto && [neighbor, weight] : adj[v]) {
+        ad(neighbor, cd + weight);
       }
-   }
+    }
+  }
 };
