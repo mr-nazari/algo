20c120d53f4591f3211aac9f49a684b594467b90
Mr. A.N <69844241+mr-nazari@users.noreply.github.com>
2023-06-06 13:56:22 +0330

    Update kruskal.cpp: 2 space

--- graph/kruskal.cpp
+++ graph/kruskal.cpp
@@ -1,12 +1,12 @@
-template <typename T> T kruskal(vector<Edge<T>> ed) {
-   const int n = (int) ed.size();
-   sort(ed.begin(), ed.end());
-   T res = 0;
-   DSU d(n);
-   for (auto& [u, v, weight] : ed) {
-      if (d.unite(u, v)) {
-         res += weight;
-      }
-   }
-   return res;
+template <typename T> T kruskal(vector<edge<T>> ed) {
+  const int n = (int) ed.size();
+  sort(ed.begin(), ed.end());
+  T res = 0;
+  dsu d(n);
+  for (auto& [u, v, weight] : ed) {
+    if (d.unite(u, v)) {
+      res += weight;
+    }
+  }
+  return res;
 }
