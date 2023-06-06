988ff03db734f45f39b317f190f623d505f41e9a
Mr. A.N <69844241+mr-nazari@users.noreply.github.com>
2023-06-06 13:58:31 +0330

    Update kmp.cpp

--- string/kmp.cpp
+++ string/kmp.cpp
@@ -1,12 +1,12 @@
 vector<int> kmp(const string& s) {
-   const int n = (int) s.size();
-   vector<int> pi(n);
-   for (int i = 1, j = 0; i < n; ++i) {
-      while (j > 0 && s[j] != s[i]) {
-         j = pi[j - 1];
-      }
-      j += (int) (s[j] == s[i]);
-      pi[i] = j;
-   }
-   return pi;
+  const int n = (int) s.size();
+  vector<int> pi(n);
+  for (int i = 1, j = 0; i < n; ++i) {
+    while (j > 0 && s[j] != s[i]) {
+      j = pi[j - 1];
+    }
+    j += (int) (s[j] == s[i]);
+    pi[i] = j;
+  }
+  return pi;
 }
