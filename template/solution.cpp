fd4882a7d6520f515ba39bb4b52bf0ce76c9ba86
Mr. A.N <69844241+mr-nazari@users.noreply.github.com>
2023-06-06 14:14:46 +0330

    Create solution.cpp

---
+++ template/solution.cpp
@@ -0,0 +1,20 @@
+/**
+ *    author:  ${NAME}
+ *    created: ${DATE}
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#endif
+
+using i64 = long long;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  ${0}
+  return 0;
+}
