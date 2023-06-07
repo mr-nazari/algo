86345e29d97383f19f153712b96b117febf53761
Mr. A.N <69844241+mr-nazari@users.noreply.github.com>
2023-06-07 14:54:28 +0330

    Create phi.cpp

---
+++ numeric/phi.cpp
@@ -0,0 +1,29 @@
+int Phi(int n) {
+  int res = n;
+  for (int d = 2; d * d <= n; ++d) {
+    if (n % d == 0) {
+      while (n % d == 0) {
+        n /= d;
+      }
+      res -= res / d;
+    }
+  }
+  if (n > 1) {
+    res -= res / n;
+  }
+  return res;
+}
+
+vector<int> phi;
+
+void PhiUpToN(int n) {
+  phi.resize(n + 1);
+  iota(phi.begin(), phi.end(), 0);
+  for (int i = 2; i <= n; ++i) {
+    if (phi[i] == i) {
+      for (int j = i; j <= n; j += i) {
+        phi[j] -= phi[j] / i;
+      }
+    }
+  }
+}
