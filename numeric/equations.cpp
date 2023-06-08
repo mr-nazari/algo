96f71eb20d56f6977230f17a924bb6ec06362589
Mr. A.N <69844241+mr-nazari@users.noreply.github.com>
2023-06-08 17:04:14 +0330

    Create equations.cpp

---
+++ numeric/equations.cpp
@@ -0,0 +1,20 @@
+template <typename T>
+T SolveLinearEquation(T a, T b) {
+  if (a == 0) {
+    throw domain_error("This isn't a linear equation.");
+  }
+  return -b / a;
+}
+ 
+template <typename T>
+pair<T, T> SolveQuadraticEquation(T a, T b, T c) {
+  T delta = b * b - 4 * a * c;
+  if (delta < 0) {
+    throw domain_error("Delta is negetive.");
+  }
+  if (a == 0) {
+    throw domain_error("This isn't a quadratic equation.");
+  }
+  return pair((-b - sqrt(delta)) / (2 * a),
+              (-b + sqrt(delta)) / (2 * a));
+}
