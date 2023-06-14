b53cc7fabfc222d2a825047d9bbe0c329a98c089
Mr. A.N <69844241+mr-nazari@users.noreply.github.com>
2023-06-14 10:22:49 +0330

    Update definitions.cpp

--- definitions.cpp
+++ definitions.cpp
@@ -15,4 +15,37 @@ template <typename T> bool ckmax(T& a, const T& b) {
    return a < b ? a = b, 1 : 0;
 }
 
+#include <bits/extc++.h>
+
+struct splitmix64_hash {
+  static uint64_t splitmix64(uint64_t x) {
+    // http://xorshift.di.unimi.it/splitmix64.c
+    x += 0x9e3779b97f4a7c15;
+    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+    return x ^ (x >> 31);
+  }
+
+  size_t operator()(uint64_t x) const {
+    static const uint64_t kFixedRandom =
+      std::chrono::steady_clock::now().time_since_epoch().count();
+    return splitmix64(x + kFixedRandom);
+  }
+};
+
+template <typename K, typename V, typename Hash = splitmix64_hash>
+using hash_map = __gnu_pbds::gp_hash_table<K, V, Hash>;
+
+template <typename K, typename Hash = splitmix64_hash>
+using hash_set = hash_map<K, __gnu_pbds::null_type, Hash>;
+
 mt19937_64 rng((unsigned int) chrono::steady_clock::now().time_since_epoch().count());
+
+int rnd(int l, int r) {
+  return uniform_int_distribution<int>(l, r)(rng);
+}
+
+double clock_begin = clock();
+auto cur_time = [&]() -> double {
+  return (clock() - clock_begin) / CLOCKS_PER_SEC;
+};
